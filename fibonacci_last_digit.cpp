902f023e34146e37a5f575d6c620000ac8d3bd96
Srilipta Swain <56768355+SriLiptaSwain@users.noreply.github.com>
2020-07-08 02:49:27 +0530

    Update fibonacci_last_digit.cpp

--- fibonacci_last_digit.cpp
+++ fibonacci_last_digit.cpp
@@ -1,19 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int get_fibonacci_last_digit_naive(int n) {
-    if (n <= 1)
-        return n;
-
-    int previous = 0;
-    int current  = 1;
-
-    for (int i = 0; i < n - 1; ++i) {
-        int tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-    }
-
-    return current % 10;
+    long long f[n+1];
+	f[0]=0;
+	f[1]=1;
+	for(int i=2;i<n+1;i++)
+		f[i]=(f[i-1]+f[i-2])%10;
+    return f[n];
 }
 
 int main() {
@@ -21,4 +15,4 @@ int main() {
     std::cin >> n;
     int c = get_fibonacci_last_digit_naive(n);
     std::cout << c << '\n';
-    }
+}
